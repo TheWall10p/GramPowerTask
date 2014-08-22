f39920a69fd291bde86920de34389e45e1f113ab
Aviral <aviral.2815@gmail.com>
2014-08-22 15:55:26 +0530

    Correct formatting

--- parsing.c
+++ parsing.c
@@ -1,46 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+
+int ParseForHello(char c);
+void NextState(char c);
+int Output ();
 
 int state=0;
-char name[]="demmda";
+char name[]="hello";
 
 int main()
 { 
-	int flag=0;
- while(1) 
- { 
- char c = getchar(); 
- int helloFound = ParseForHello(c); 
- if(helloFound) 
- { 
-	break;
- } 
- } 
- return 0;
+	 int flag=0;
+	 while(1) 
+	 { 
+		 char c = getchar(); 
+		 int helloFound = ParseForHello(c); 
+		 if(helloFound) 
+		 { 
+			break;
+		 } 
+	 } 
+	 return 0;
 }
 
-int ParseForHello(c){
-state = NextState(c,state);
-int Out=Output(state);
-return Out;
+int ParseForHello(char c)
+{
+	NextState(c);
+	return Output();
 }
 
-int NextState(c,state){
-if(c==name[0] && !(c==name[state])) {
-state=1;
-}
-else if (state==6) {
-state=0;
-}
-else if(c==name[state]){
-state++;
-}
-else state=0;
-return state;
+void NextState(char c){
+	if(c==name[0] && !(c==name[state]))
+	{
+		state=1;
+	}
+	else if (state==6) 
+	{
+		state=0;
+	}
+	else if(c==name[state])
+	{
+		state++;
+	}
+	else state=0;
 }
 
-int Output (state) {
-int Out;
-if(state==6) Out=1;
-else Out=0;
-return Out;
+int Output () 
+{
+	return (state==strlen(name));
 }
